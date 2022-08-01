dfd8a9be5dcb0788d073e32d53fd244782ab5aef
marudengi <58734611+marudengi@users.noreply.github.com>
2022-08-01 15:50:04 +0900

    Update config.js

--- Backend/node-api/config.js
+++ Backend/node-api/config.js
@@ -1,19 +1,8 @@
-const env = process.env.NODE_ENV; // 'dev' or 'test'
-
-const dev = {
- app: {
-   port: parseInt(process.env.DEV_APP_PORT) || 3000
- }
-};
-const test = {
- app: {
-   port: parseInt(process.env.TEST_APP_PORT) || 3000
- }
-};
-
 const config = {
- dev,
- test
+ app: {
+  port: 3000,
+  host: '0.0.0.0'
+ }
 };
 
-module.exports = config[env];
+module.exports = config;
